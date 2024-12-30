eac2be58f7a8a17ca5640de4ae64815036971437
jimmy5057 <wj.tan@webnic.cc>
2024-12-30 16:57:05 +0800

    draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Website Loaded');
   });
+
+document.querySelector('.chat-container').addEventListener('click', function() {
+    // Add your chat opening logic here
+});
